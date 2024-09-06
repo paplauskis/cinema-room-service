b88c1f2ad3d9c5eddb52688395d13b6839db5b01
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 12:17:25 +0300

    added purchaseSeat method to ...purchase a seat

--- src/main/java/org/example/cinemaroomservice/services/CinemaService.java
+++ src/main/java/org/example/cinemaroomservice/services/CinemaService.java
@@ -1,5 +1,8 @@
 package org.example.cinemaroomservice.services;
 
+import org.example.cinemaroomservice.exceptions.InvalidSeatNumberException;
+import org.example.cinemaroomservice.exceptions.SeatAlreadyPurchasedException;
+import org.example.cinemaroomservice.exceptions.SeatNotFoundException;
 import org.example.cinemaroomservice.models.Cinema;
 import org.example.cinemaroomservice.models.Seat;
 import org.springframework.stereotype.Service;
@@ -18,7 +21,7 @@ public class CinemaService {
 
         for (int row = 1; row <= rowCount; row ++) {
             for (int column = 1; column <= columnCount; column++) {
-                seatsList.add(new Seat(row, column));
+                seatsList.add(new Seat(row, column, row <= 4 ? 10 : 8));
             }
         }
 
@@ -27,5 +30,23 @@ public class CinemaService {
 
     public Cinema getCinema() {
         return this.cinema;
+    }
+
+    public Seat purchaseSeat(int row, int column) throws InvalidSeatNumberException, SeatAlreadyPurchasedException, SeatNotFoundException {
+        int rowCount = cinema.getRowCount();
+        int columnCount = cinema.getColumnCount();
+
+        if (row < 1 || row > rowCount || column < 1 || column > columnCount) {
+            throw new InvalidSeatNumberException("The number of a row or a column is out of bounds!");
+        }
+
+        Seat seat = cinema.getSeat(row, column);
+
+        if (seat.isPurchased()) {
+            throw new SeatAlreadyPurchasedException("The ticket has been already purchased!");
+        }
+
+        seat.purchase();
+        return seat;
     }
 }
