3cb74ecb79ec991079362c3201980c159b9b3715
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 16:26:36 +0300

    changed to record

--- src/main/java/org/example/cinemaroomservice/dto/ErrorResponseDTO.java
+++ src/main/java/org/example/cinemaroomservice/dto/ErrorResponseDTO.java
@@ -1,17 +1,4 @@
 package org.example.cinemaroomservice.dto;
 
-public class ErrorResponseDTO {
-    private String error;
-
-    public ErrorResponseDTO(String error) {
-        this.error = error;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
+public record ErrorResponseDTO(String error) {
 }
