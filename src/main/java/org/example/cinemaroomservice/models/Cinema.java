27425e9fb2ec5ece0315b7205948c4bb07e15149
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 18:54:38 +0300

    moved getSeat to CinemaService

--- src/main/java/org/example/cinemaroomservice/models/Cinema.java
+++ src/main/java/org/example/cinemaroomservice/models/Cinema.java
@@ -30,13 +30,4 @@ public class Cinema {
     public List<Seat> getSeatsList() {
         return this.seatsList;
     }
-
-    public Seat getSeat(int row, int column) throws SeatNotFoundException {
-        for (Seat seat : seatsList) {
-            if (seat.getRow() == row && seat.getColumn() == column) {
-                return seat;
-            }
-        }
-        throw new SeatNotFoundException("Cannot find seat");
-    }
 }
