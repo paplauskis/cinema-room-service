8c016e6e45be496435c5ba38dfb34e00ae19f165
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 12:21:48 +0300

    added @ComponentScan annotation so Spring Boot finds the root of all files

--- src/main/java/org/example/cinemaroomservice/application/CinemaRoomServiceApplication.java
+++ src/main/java/org/example/cinemaroomservice/application/CinemaRoomServiceApplication.java
@@ -2,8 +2,10 @@ package org.example.cinemaroomservice.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.example.cinemaroomservice")
 public class CinemaRoomServiceApplication {
 
     public static void main(String[] args) {
