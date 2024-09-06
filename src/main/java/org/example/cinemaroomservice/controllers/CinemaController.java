6443729d648c3a3a3722dd3b00b20b0a40979877
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 12:20:45 +0300

    added /purchase endpoint; returns seat to be purchased

--- src/main/java/org/example/cinemaroomservice/controllers/CinemaController.java
+++ src/main/java/org/example/cinemaroomservice/controllers/CinemaController.java
@@ -1,13 +1,15 @@
 package org.example.cinemaroomservice.controllers;
 
+import org.example.cinemaroomservice.dto.ErrorResponseDTO;
+import org.example.cinemaroomservice.dto.SeatPurchaseRequestDTO;
+import org.example.cinemaroomservice.exceptions.*;
 import org.example.cinemaroomservice.models.Cinema;
+import org.example.cinemaroomservice.models.Seat;
 import org.example.cinemaroomservice.services.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +21,16 @@ public class CinemaController {
     @GetMapping("/seats")
     ResponseEntity<Cinema> getSeats() {
         return new ResponseEntity<>(cinemaService.getCinema(), HttpStatus.OK);
+    }
+
+    @PostMapping("/purchase")
+    ResponseEntity<?> getPurchasedSeat(@RequestBody SeatPurchaseRequestDTO request) {
+        try {
+            Seat purchasedSeat = cinemaService.purchaseSeat(request.getRow(), request.getColumn());
+            return new ResponseEntity<>(purchasedSeat, HttpStatus.OK);
+        } catch (InvalidSeatNumberException | SeatNotFoundException | SeatAlreadyPurchasedException e) {
+            ErrorResponseDTO errorResponse = new ErrorResponseDTO(e.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
     }
 }
