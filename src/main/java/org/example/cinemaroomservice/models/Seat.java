56e26b6e1e593988ac6ebd5a9924d64e561b7cb9
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 12:18:18 +0300

    added getPrice, isPurchased and purchase methods

--- src/main/java/org/example/cinemaroomservice/models/Seat.java
+++ src/main/java/org/example/cinemaroomservice/models/Seat.java
@@ -1,12 +1,18 @@
 package org.example.cinemaroomservice.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Seat {
     private final int row;
     private final int column;
+    private final int price;
+    private boolean isPurchased;
 
-    public Seat(int row, int column) {
+    public Seat(int row, int column, int price) {
         this.row = row;
         this.column = column;
+        this.price = price;
+        this.isPurchased = false;
     }
 
     public int getRow() {
@@ -15,5 +21,18 @@ public class Seat {
 
     public int getColumn() {
         return this.column;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @JsonIgnore
+    public boolean isPurchased() {
+        return isPurchased;
+    }
+
+    public void purchase() {
+        this.isPurchased = true;
     }
 }
