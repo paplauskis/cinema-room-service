fcea462d2c138be6ce9cdbd18143ee9123b46689
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 16:26:55 +0300

    changed to record

--- src/main/java/org/example/cinemaroomservice/dto/SeatPurchaseRequestDTO.java
+++ src/main/java/org/example/cinemaroomservice/dto/SeatPurchaseRequestDTO.java
@@ -1,19 +1,4 @@
 package org.example.cinemaroomservice.dto;
 
-public class SeatPurchaseRequestDTO {
-    private final int row;
-    private final int column;
-
-    public SeatPurchaseRequestDTO(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
+public record SeatPurchaseRequestDTO(int row, int column) {
 }
