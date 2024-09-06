f92730f0ef309d5ed8556ac5ef1803d9bef0561d
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 19:00:41 +0300

    added /return endpoint, it returns ticket found by id

--- src/main/java/org/example/cinemaroomservice/controllers/CinemaController.java
+++ src/main/java/org/example/cinemaroomservice/controllers/CinemaController.java
@@ -1,11 +1,14 @@
 package org.example.cinemaroomservice.controllers;
 
 import org.example.cinemaroomservice.dto.ErrorResponseDTO;
+import org.example.cinemaroomservice.dto.IdRequestDTO;
 import org.example.cinemaroomservice.dto.SeatPurchaseRequestDTO;
 import org.example.cinemaroomservice.exceptions.*;
 import org.example.cinemaroomservice.models.Cinema;
 import org.example.cinemaroomservice.models.Seat;
+import org.example.cinemaroomservice.models.Ticket;
 import org.example.cinemaroomservice.services.CinemaService;
+import org.example.cinemaroomservice.services.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,8 @@ public class CinemaController {
 
     @Autowired
     private CinemaService cinemaService;
+    @Autowired
+    private TicketService ticketService;
 
     @GetMapping("/seats")
     ResponseEntity<Cinema> getSeats() {
@@ -29,6 +34,17 @@ public class CinemaController {
             Seat purchasedSeat = cinemaService.purchaseSeat(request.row(), request.column());
             return new ResponseEntity<>(purchasedSeat, HttpStatus.OK);
         } catch (InvalidSeatNumberException | SeatNotFoundException | SeatAlreadyPurchasedException e) {
+            ErrorResponseDTO errorResponse = new ErrorResponseDTO(e.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/return")
+    ResponseEntity<?> getPurchasedTicket(@RequestBody IdRequestDTO requestId) {
+        try {
+            Ticket ticket = ticketService.findTicketById(requestId.id());
+            return new ResponseEntity<>(ticket, HttpStatus.OK);
+        } catch (TicketNotFoundException e) {
             ErrorResponseDTO errorResponse = new ErrorResponseDTO(e.getMessage());
             return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         }
