3566bd1f9471c00661e9183af2804fe23c98f650
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-06 12:22:39 +0300

    created DTO so it can send back errors in JSON

---
+++ src/main/java/org/example/cinemaroomservice/dto/ErrorResponseDTO.java
@@ -0,0 +1,17 @@
+package org.example.cinemaroomservice.dto;
+
+public class ErrorResponseDTO {
+    private String error;
+
+    public ErrorResponseDTO(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
