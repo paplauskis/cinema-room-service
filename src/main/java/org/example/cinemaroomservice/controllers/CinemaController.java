15df6a85c28e6e739a17f632c80f0fe276c95861
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-09-16 22:36:06 +0300

    added /stats endpoint to return stats about cinema

--- src/main/java/org/example/cinemaroomservice/controllers/CinemaController.java
+++ src/main/java/org/example/cinemaroomservice/controllers/CinemaController.java
@@ -1,5 +1,6 @@
 package org.example.cinemaroomservice.controllers;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.example.cinemaroomservice.dto.ErrorResponseDTO;
 import org.example.cinemaroomservice.dto.IdRequestDTO;
 import org.example.cinemaroomservice.dto.SeatPurchaseRequestDTO;
@@ -8,8 +9,10 @@ import org.example.cinemaroomservice.models.Cinema;
 import org.example.cinemaroomservice.models.Seat;
 import org.example.cinemaroomservice.models.Ticket;
 import org.example.cinemaroomservice.services.CinemaService;
+import org.example.cinemaroomservice.services.StatsService;
 import org.example.cinemaroomservice.services.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +25,8 @@ public class CinemaController {
     private CinemaService cinemaService;
     @Autowired
     private TicketService ticketService;
+    @Autowired
+    private StatsService statsService;
 
     @GetMapping("/seats")
     ResponseEntity<Cinema> getSeats() {
@@ -32,6 +37,7 @@ public class CinemaController {
     ResponseEntity<?> getPurchasedSeat(@RequestBody SeatPurchaseRequestDTO request) {
         try {
             Seat purchasedSeat = cinemaService.purchaseSeat(request.row(), request.column());
+            statsService.edit(purchasedSeat.getTicket().getPrice(), true);
             return new ResponseEntity<>(purchasedSeat, HttpStatus.OK);
         } catch (InvalidSeatNumberException | SeatNotFoundException | SeatAlreadyPurchasedException e) {
             ErrorResponseDTO errorResponse = new ErrorResponseDTO(e.getMessage());
@@ -43,8 +49,23 @@ public class CinemaController {
     ResponseEntity<?> getPurchasedTicket(@RequestBody IdRequestDTO requestId) {
         try {
             Ticket ticket = ticketService.findTicketById(requestId.id());
+            statsService.edit(ticket.getPrice(), false);
             return new ResponseEntity<>(ticket, HttpStatus.OK);
         } catch (TicketNotFoundException e) {
+            ErrorResponseDTO errorResponse = new ErrorResponseDTO(e.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/stats")
+    ResponseEntity<?> getStats(@RequestParam String password) {
+        try {
+            String secret_password = Dotenv.load().get("PASSWORD");
+            if (password.equals(secret_password)) {
+                return new ResponseEntity<>(statsService.getStats(), HttpStatus.OK);
+            }
+            throw new WrongAdminPasswordException("Invalid password!");
+        } catch (WrongAdminPasswordException e) {
             ErrorResponseDTO errorResponse = new ErrorResponseDTO(e.getMessage());
             return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         }
